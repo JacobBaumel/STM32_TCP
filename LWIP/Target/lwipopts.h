47c064e5c34052a1bde39abd753f5e9ec81389ba
Jacob Baumel <jacob.a.baumel@gmail.com>
2026-05-15 14:31:55 -0500

    Did not actually need nonintrinsic memcpy

--- LWIP/Target/lwipopts.h
+++ LWIP/Target/lwipopts.h
@@ -139,9 +139,6 @@ extern "C" {
     }                                                                                                                  \
     while(0)
 
-// See nonintrinsic_memcpy.c
-void* nonintrinsic_memcpy(void* dest, const void* src, size_t bytes);
-#define SMEMCPY(dest, src, len) nonintrinsic_memcpy(dest, src, len)
 /* USER CODE END 1 */
 
 #ifdef __cplusplus
